b03a82b3d641a3a265759cbfa3eb9ba1acab4a7e
dung-nguyen-huu <dung99xcyd@gmail.com>
2020-10-12 15:11:57 +0700

    temple

--- src/server.js
+++ src/server.js
@@ -1,27 +1,26 @@
 import express from "express";
 import ConnectDB from "./config/connectDB";
 import ContactModel from "./model/contact.model";
-import env from "../evn/environment";
+import ConfigViewEngine from "./config/viewEngine";
 
+// khởi tại app
 let app = express();
 
 // kết nối với MongoDB
-ConnectDB();
+// ConnectDB();
+
+// tạo view engine
+ConfigViewEngine(app);
 
 let hostname = "localhost";
 let port = 8888;
 
-app.get("/testDB", async (req, res) => {
-    try {
-        let item = {
-            userId: "1999",
-            contactId: "2000",
-        };
-        let contact = await ContactModel.createNew(item);
-        res.send(JSON.stringify(contact));
-    } catch (error) {
-        console.log(error);        
-    }
+app.get("/", (req, res) => {
+    return res.render("main/master");
+});
+
+app.get("/login-register", (req, res) => {
+    return res.render("auth/loginRegister");
 });
 
 app.listen(port, hostname, () => {
